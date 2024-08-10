4e5aff5aa85a451704b1a3cb389d02dd524e1a66
Toheed Asghar <toheedasghar1@gmail.com>
2024-08-10 23:32:32 +0500

    Layout Page is added. Some Errors are commented. Error: The Images (icons) on footer and header are not being showed on the brower.

--- Gadget Gourmet/obj/Debug/net8.0/Gadget Gourmet.AssemblyInfo.cs
+++ Gadget Gourmet/obj/Debug/net8.0/Gadget Gourmet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gadget Gourmet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553de338b880357317788b14a81ac7105f8e7944")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gadget Gourmet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gadget Gourmet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
