7fd23652ed3e4becb10de3fde1c451e609ec3754
Toheed Asghar <toheedasghar1@gmail.com>
2024-08-11 15:41:13 +0500

    Following things have been added: a) privacy policy b) home page (2 sections)

--- Gadget Gourmet/obj/Debug/net8.0/Gadget Gourmet.AssemblyInfo.cs
+++ Gadget Gourmet/obj/Debug/net8.0/Gadget Gourmet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gadget Gourmet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c825bf5acf9a365a49401360faaaa37957cb83b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f6c01c3b1edc9cc9797f117048c4ac44f762810")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gadget Gourmet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gadget Gourmet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
