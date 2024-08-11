1d0b9c0416bab36814448a980333574b7c948d03
unknown <huzaifach1981@gmail.com>
2024-08-11 03:59:38 -0700

    Things added: 1) Entities 2) Tables 3) Interfaces 4) Repository

--- Gadget Gourmet/Models/Entities/User.cs
+++ Gadget Gourmet/Models/Entities/User.cs
@@ -1,7 +1,26 @@
 ﻿namespace Gadget_Gourmet.Models.Entities
 {
-	public class User
-	{
-        public int MyProperty { get; set; }
+    public class User
+    {
+        public string? Name { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? Gender { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+
+        public User(string? name, string? username, string? password, string? email, string? address, string? phone, string? gender, DateOnly dateofbirth)
+        {
+            Name = name;
+            UserName = username;
+            Password = password;
+            Email = email;
+            Address = address;
+            Phone = phone;
+            Gender = gender;
+            DateOfBirth = dateofbirth;
+        }
     }
 }
---
+++ Gadget Gourmet/Models/Interface/IUser.cs
@@ -0,0 +1,11 @@
+﻿using Gadget_Gourmet.Models.Entities;
+namespace Gadget_Gourmet.Models.Interface
+{
+    public interface IUser
+    {
+        bool Login(User user);
+        bool Signup(User user);
+        bool PersonalInfo(User user);
+        User GetUserByUserName(string? un);
+    }
+}
